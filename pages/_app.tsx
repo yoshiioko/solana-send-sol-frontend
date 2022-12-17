67ee7242992457ff13ecc898d20031448ef51ae8
yoshiioko <acardoza@pdx.edu>
2022-12-16 18:03:11 -0800

    copying template code to initialize wallet and network tools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,36 @@
-import '../styles/globals.css'
+import React, { useMemo } from "react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react"
+import { GlowWalletAdapter, PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+require("@solana/wallet-adapter-react-ui/styles.css");
+require("../styles/globals.css");
+require("../styles/Home.module.css");
 
-export default MyApp
+const App = ({ Component, pageProps }) => {
+  const network = WalletAdapterNetwork.Devnet;
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new GlowWalletAdapter(),
+      new SolflareWalletAdapter()
+    ], [network]
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default App;
