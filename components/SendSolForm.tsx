7251fc637ac48e9a6b68fc6e60d9bbce13d683d6
yoshiioko <acardoza@pdx.edu>
2022-12-16 19:12:47 -0800

    implement code to create transaction to send SOL to other account

--- components/SendSolForm.tsx
+++ components/SendSolForm.tsx
@@ -1,12 +1,33 @@
+import { useConnection, useWallet } from '@solana/wallet-adapter-react'
+import * as Web3 from "@solana/web3.js"
 import { FC } from 'react'
 import styles from '../styles/Home.module.css'
 
 
 export const SendSolForm: FC = () => {
-
+    const { connection } = useConnection();
+    const { publicKey, sendTransaction } = useWallet();
+    
     const sendSol = event => {
+        if (!connection || !publicKey) {
+            alert("Please connect your wallet first...")
+            return
+        }
+
         event.preventDefault()
         console.log(`Send ${event.target.amount.value} SOL to ${event.target.recipient.value}`)
+
+        const transaction = new Web3.Transaction();
+        const instruction = Web3.SystemProgram.transfer({
+            fromPubkey: publicKey,
+            toPubkey: new Web3.PublicKey(event.target.recipient.value),
+            lamports: Web3.LAMPORTS_PER_SOL * event.target.amount.value,
+        })
+
+        transaction.add(instruction);
+        sendTransaction(transaction, connection).then(sig => {
+            console.log(`Explorer URL: https://explorer.solana.com/tx/${sig}?cluster=devnet`)
+        })
     }
 
     return (
